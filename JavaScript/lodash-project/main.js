e6eec999405f2de618bf387c9d391a21c43ec144
Alan Azocar <a.franco.azocar@gmail.com>
2024-04-24 18:49:22 -0300

    lodash-project

--- JavaScript/lodash-project/main.js
+++ JavaScript/lodash-project/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import _, { zip } from "lodash";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// dividir un array en segmentos
 
-setupCounter(document.querySelector('#counter'))
+const arraySegmentado = _.chunk(["a","b","c","d","e","f"],4);
+console.log("Segmentado en array de 2: ",arraySegmentado);
+
+const aleatorio = _.sample([1,2,3,4,5]);
+console.log("Numero aleatorio:",aleatorio);
+
+const shuffle= _.shuffle([1,2,3,4,5,6,7,8,9]);
+console.log("Array aleatorio:",shuffle);
+
+const duplicados = _. map([1, 2, 3, 4, 5], (n) => n* 2);
+console.log("Duplicados: ", duplicados);
+

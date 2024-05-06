faaf41c449b2be0d893625b62c396986d5f6eb97
Alan Azocar <a.franco.azocar@gmail.com>
2024-05-06 19:35:35 -0300

    dasda

--- JavaScript/Clase-21/main.js
+++ JavaScript/Clase-21/main.js
@@ -1,7 +1,8 @@
 import './style.css';
 import { showAlert } from './modules/alerts';
 import { showNotification } from './modules/notification';
-
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
 
 document.querySelector("#showAlert").addEventListener("click", () =>{
   showAlert();
